fd800f9d5dc407a89d1dfd6c8f1ef2deaabbafc5
SayaniSen2022 <109034346+SayaniSen2022@users.noreply.github.com>
2024-11-02 13:19:24 +0530

    group anagram added

--- 2025prep/groupAnagrams.js
+++ 2025prep/groupAnagrams.js
@@ -1,1 +1,14 @@
-// leet 49
+// leet 49 : hashmap TC: O(mnlogn) ; SC: O(mn) Where m is the number of strings and n is the length of the longest string.
+
+var groupAnagrams = function (strs) {
+  const res = {};
+
+  for (let s of strs) {
+    const sortedS = s.split("").sort().join();
+    if (!res[sortedS]) {
+      res[sortedS] = [];
+    }
+    res[sortedS].push(s);
+  }
+  return Object.values(res);
+};
